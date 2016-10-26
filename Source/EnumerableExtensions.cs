6d76366395b52283d500352445fca3b628ac3db7
David Makovský <david.makovsky@avg.com>
2016-10-26 22:46:12 +0200

    feat: Enumerable<T>.ForEach missing files

---
+++ Source/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace System.Collections.Generic
+{
+	using CommonNet.Extensions;
+	using System;
+
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		///   Performs action on each element of the <see cref="IEnumerable{TValue}"/>.
+		///   This function allows using the same expressions like when using <see cref="List{TValue}.ForEach(Action{TValue})"/>.
+		/// </summary>
+		/// <param name="self">Enumerable where to perform action on elements.</param>
+		/// <param name="action"><see cref="Action{TValue}"/> delegate to perform on each element.</param>
+		public static void ForEach<TValue>(
+			this IEnumerable<TValue> self,
+			Action<TValue> action
+		)
+		{
+			Check.Self(self);
+			Check.Argument(action, nameof(action));
+
+			foreach (TValue item in self)
+				action(item);
+		}
+	}
+}
