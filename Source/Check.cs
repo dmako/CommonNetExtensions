fd9fd04f2600fb7fb37f383b54e58999f8ef2030
David Makovský <david.makovsky@gmail.com>
2016-10-26 21:41:27 +0200

    Dictionary extensions and basic tests (#1)
    feat: dictionary extensions and basic tests
    
    * documentation + tests skeletons
    
    * dict + tests
    
    * assembly info

---
+++ Source/Check.cs
@@ -0,0 +1,46 @@
+﻿namespace CommonNet.Extensions
+{
+	using System;
+	using System.IO;
+	using System.Runtime.CompilerServices;
+
+	internal static class Check
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Self(object selfArg)
+		{
+			if (selfArg == null)
+				throw new ArgumentNullException(nameof(selfArg));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Argument(object arg, string argName)
+		{
+			if (arg == null)
+				throw new ArgumentNullException(argName);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void ArgumentWithMsg(object arg, string argName, string msg)
+		{
+			if (arg == null)
+				throw new ArgumentNullException($"{argName}: {msg}");
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void ArgumentIsRootedPath(string arg, string argName)
+		{
+			Check.Argument(arg, argName);
+
+			if (!Path.IsPathRooted(arg))
+				throw new ArgumentException($"{argName} is not rooted path.");
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Verify(bool result, string message)
+		{
+			if (!result)
+				throw new InvalidDataException(message);
+		}
+	}
+}
---
+++ Tests/DictionaryExtensions.cs
@@ -0,0 +1,64 @@
+﻿namespace Tests
+{
+	using NUnit.Framework;
+	using System;
+	using System.Collections.Generic;
+
+	[TestFixture]
+	public class DictionaryExtensions
+	{
+		readonly Func<int, int> factoryFnc = (key) => key;
+		readonly Func<int, int> factoryFncPlusOne = (key) => key + 1;
+		readonly Func<int, int, int> updateFnc = (key, old) => old + key;
+
+		[Test]
+		public void Dictionary_AddOrGetValueBasicTest()
+		{
+			var dict = new Dictionary<int, int>();
+			var val = dict.GetOrAdd(1, 1);
+			Assert.AreEqual(1, val);
+			val = dict.GetOrAdd(1, 2);
+			Assert.AreEqual(1, val);
+
+			val = dict.GetOrAdd(2, factoryFnc);
+			Assert.AreEqual(factoryFnc(2), val);
+			val = dict.GetOrAdd(2, factoryFncPlusOne);
+			Assert.AreEqual(factoryFnc(2), val);
+		}
+
+		[Test]
+		public void Dictionary_AddOrGetLazyValueBasicTest()
+		{
+			var dict = new Dictionary<int, Lazy<int>>();
+			var val = dict.GetOrAdd(3, factoryFnc);
+			Assert.AreEqual(factoryFnc(3), val);
+			val = dict.GetOrAdd(3, factoryFncPlusOne);
+			Assert.AreEqual(factoryFnc(3), val);
+		}
+
+		[Test]
+		public void Dictionary_AddOrUpdateValueBasicTest()
+		{
+			var dict = new Dictionary<int, int>();
+			var val = dict.AddOrUpdate(1, 1, updateFnc);
+			Assert.AreEqual(1, val);
+			val = val = dict.AddOrUpdate(1, 1, updateFnc);
+			Assert.AreEqual(updateFnc(1, 1), val);
+
+			val = dict.AddOrUpdate(2, factoryFnc, updateFnc);
+			Assert.AreEqual(factoryFnc(2), val);
+			val = val = dict.AddOrUpdate(2, factoryFnc, updateFnc);
+			Assert.AreEqual(updateFnc(2, 2), val);
+		}
+
+		[Test]
+		public void Dictionary_AddOrUpdateLazyValueBasicTest()
+		{
+			var dict = new Dictionary<int, Lazy<int>>();
+			var val = dict.AddOrUpdate(3, factoryFnc, updateFnc);
+			Assert.AreEqual(factoryFnc(3), val);
+			val = val = dict.AddOrUpdate(3, factoryFnc, updateFnc);
+			Assert.AreEqual(updateFnc(3, 3), val);
+		}
+	}
+}
