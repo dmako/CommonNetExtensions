c098b8e56b73b440d04b02660f48fe12eada8d16
David Makovský <david.makovsky@avg.com>
2016-10-26 12:30:25 +0200

    backup

---
+++ Source/Check.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace CommonNet.Extensions
+{
+	public static class Check
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Self(object selfArg)
+		{
+			if (selfArg == null)
+				throw new ArgumentNullException(nameof(selfArg));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Argument(object arg, string argName)
+		{
+			if (arg == null)
+				throw new ArgumentNullException(argName);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Argument(string arg, string argName)
+		{
+			if (String.IsNullOrWhiteSpace(arg))
+				throw new ArgumentNullException(argName);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void ArgumentWithMsg(object arg, string argName, string msg)
+		{
+			if (arg == null)
+				throw new ArgumentNullException($"{argName}: {msg}");
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void ArgumentWithMsg(string arg, string argName, string msg)
+		{
+			if (String.IsNullOrWhiteSpace(arg))
+				throw new ArgumentNullException($"{argName}: {msg}");
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void ArgumentIsRootedPath(string arg, string argName)
+		{
+			Check.Argument(arg, argName);
+
+			if (!Path.IsPathRooted(arg))
+				throw new ArgumentException($"{argName} is not rooted path.");
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Verify(bool result, string message)
+		{
+			if (!result)
+				throw new InvalidDataException(message);
+		}
+	}
+}
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+[assembly: AssemblyTitle("CommonNetExtensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommonNetExtensions")]
+[assembly: AssemblyCopyright("Copyright © David Makovský 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
