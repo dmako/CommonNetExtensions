73edf247cb198001739c5ae1f54cb7118283dbca
David Makovský <david.makovsky@gmail.com>
2016-11-02 11:11:12 +0100

    Text reader (#2)
    * feat: Enumerable<T>.ForEach and tests
    * feat: TextReader extensions and tests

---
+++ Source/TextReaderExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace System.IO
+{
+	using Collections.Generic;
+	using CommonNet.Extensions;
+
+	public static class TextReaderExtensions
+	{
+		/// <summary>
+		///   Iterates over lines by using <see cref="TextReader"/> instance without writing while cycle code.
+		/// </summary>
+		/// <param name="self"><see cref="TextReader"/> instance to use for retrieving lines.</param>
+		/// <returns><see cref="IEnumerable{String}"/> yielded lines.</returns>
+		public static IEnumerable<string> EnumLines(this TextReader self)
+		{
+			Check.Self(self);
+
+			string line = null;
+			while ((line = self.ReadLine()) != null)
+				yield return line;
+		}
+
+		/// <summary>
+		///   Performs <see cref="Action{String}"/> on every line while iterating over lines by using <see cref="TextReader"/> instance.
+		/// </summary>
+		/// <param name="self"><see cref="TextReader"/> instance to use for retrieving lines.</param>
+		/// <param name="action"><see cref="Action{String}"/> to be performed.</param>
+		public static void ForEachLine(this TextReader self, Action<string> action)
+		{
+			Check.Self(self);
+			Check.Argument(action, nameof(action));
+
+			self.EnumLines().ForEach(line => action(line));
+		}
+
+	}
+}
---
+++ Tests/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Tests
+{
+	using NUnit.Framework;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	[TestFixture]
+	public class EnumerableExtensions
+	{
+
+		[Test]
+		public void Enumerable_ForEachBasicTest()
+		{
+
+			var data = new object[] {};
+			Assert.Catch<ArgumentNullException>(() => ((object[])null).ForEach(o => { }));
+			Assert.Catch<ArgumentNullException>(() => data.ForEach(null));
+		}
+
+		[FsCheck.NUnit.Property(MaxTest = 100, Description = nameof(Enumerable_PropertyForEachTest), QuietOnSuccess = true)]
+		public void Enumerable_PropertyForEachTest(int[] data)
+		{
+			var sum = 0;
+			data.ForEach(v => sum += v);
+			Assert.AreEqual(sum, data.Sum());
+		}
+	}
+}
