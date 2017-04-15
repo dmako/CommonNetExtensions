ec9b971ab311e9d00aec38dbf44fa04715da5f13
David Makovský <david.makovsky@avg.com>
2017-04-15 23:48:05 +0200

    feat: StringBuilder extension AppendIf

---
+++ Tests/StringBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Tests
+{
+    using NUnit.Framework;
+    using System.Text;
+    using System;
+
+    [TestFixture]
+    public class StringBuilderExtensions
+    {
+        [Test]
+        public void StringBuilde_AppendIf()
+        {
+            StringBuilder nullSb = null;
+            Assert.Catch<ArgumentException>(() => nullSb.AppendIf(1 > 0, "null"));
+
+            var sb = new StringBuilder();
+            var type = typeof(StringBuilderExtensions);
+
+            sb.AppendIf(type.IsNotPublic, "private")
+                .AppendIf(type.IsPublic, "public")
+                .Append(' ')
+                .AppendIf(type.IsClass, "class")
+                .AppendIf(type.IsByRef, "struct")
+                .Append(' ')
+                .Append(type.Name);
+
+            Assert.AreEqual(sb.ToString(), $"public class {nameof(StringBuilderExtensions)}");
+        }
+
+    }
+}
