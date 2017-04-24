9b8864fdf62eb59cde9f1621771b00be0e648777
David Makovský <david.makovsky@avg.com>
2017-04-24 22:43:55 +0200

    chore: disable few rules to keep global supression file clean

--- Source/GlobalSuppressions.cs
+++ Source/GlobalSuppressions.cs
@@ -1,3 +1,1 @@
-﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "System")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "System.Collections.Generic")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "System.IO")]
+﻿
