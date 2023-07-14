75e19d4459edb14b448a4cd7726a4ff816f56bfe
David Makovsky <david.makovsky@gmail.com>
2023-07-14 22:11:03 +0200

    feat: netstandard2.0 polyfills, resource stream extensions

--- CommonNet.Extensions/System.IO/TextReaderExtensions.cs
+++ CommonNet.Extensions/System.IO/TextReaderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using CommunityToolkit.Diagnostics;
 
 namespace System.IO;
@@ -7,37 +8,60 @@ namespace System.IO;
 /// Commonly used extension methods on <see cref="TextReader"/>.
 /// </summary>
 [EditorBrowsable(EditorBrowsableState.Never)]
-public static class sTextReaderExtensions
+public static class TextReaderExtensions
 {
     /// <summary>
     /// Iterates over lines by using <see cref="TextReader"/> instance without writing while cycle code.
     /// </summary>
-    /// <param name="self"><see cref="TextReader"/> instance to use for retrieving lines.</param>
+    /// <param name="readed"><see cref="TextReader"/> instance to use for retrieving lines.</param>
     /// <returns><see cref="IEnumerable{String}"/> yielded lines.</returns>
     public static IEnumerable<string> EnumLines(
-        this TextReader self
+        this TextReader readed
     )
     {
-        Guard.IsNotNull(self);
+        Guard.IsNotNull(readed);
 
         string? line;
-        while ((line = self.ReadLine()) != null)
+        while ((line = readed.ReadLine()) != null)
             yield return line;
+    }
+
+    /// <summary>
+    /// Iterates over lines by using <see cref="TextReader"/> instance without writing while cycle code.
+    /// </summary>
+    /// <param name="reader"><see cref="TextReader"/> instance to use for retrieving lines.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns><see cref="IEnumerable{String}"/> yielded lines.</returns>
+    public static async IAsyncEnumerable<string> EnumLinesAsync(
+        this TextReader reader,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default
+    )
+    {
+        Guard.IsNotNull(reader);
+
+        string? line;
+#if NET7_0_OR_GREATER
+        while ((line = await reader.ReadLineAsync(cancellationToken)) is not null)
+            yield return line!;
+#else
+        while ((line = await reader.ReadLineAsync()) is not null)
+            yield return line!;
+#endif
     }
 
     /// <summary>
     /// Performs <see cref="Action{String}"/> on every line while iterating over lines by using <see cref="TextReader"/> instance.
     /// </summary>
-    /// <param name="self"><see cref="TextReader"/> instance to use for retrieving lines.</param>
+    /// <param name="readed"><see cref="TextReader"/> instance to use for retrieving lines.</param>
     /// <param name="action"><see cref="Action{String}"/> to be performed.</param>
     public static void ForEachLine(
-        this TextReader self,
+        this TextReader readed,
         Action<string> action
     )
     {
-        Guard.IsNotNull(self);
+        Guard.IsNotNull(readed);
         Guard.IsNotNull(action);
 
-        self.EnumLines().ForEach(line => action(line));
+        readed.EnumLines().ForEach(line => action(line));
     }
 }
---
+++ CommonNet.Extensions/System.Reflection/EmbeddedResourceStreamReader.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using CommunityToolkit.Diagnostics;
+
+namespace System.Reflection;
+
+/// <summary>
+/// Extension methods for working with <see cref="Assembly"/> embedded manifest stream.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class EmbeddedResourceStreamExtensions
+{
+    private static string ExpandResourceName(this Assembly assembly, string resourceName)
+    {
+        var manifestResourceNames = assembly.GetManifestResourceNames();
+        if (manifestResourceNames.Any(name => name.EndsWith(resourceName)))
+        {
+            return manifestResourceNames.Single(str => str.EndsWith(resourceName));
+        }
+        return resourceName;
+    }
+
+    /// <summary>
+    /// Get Embedded Resource Stream from assembly.
+    /// </summary>
+    /// <param name="assembly">Assembly that contains the embedded resource stream.</param>
+    /// <param name="resourceName">Full or partial name of the resource.</param>
+    /// <returns>The embedded stream.</returns>
+    public static Stream GetEmbeddedResourceStream(this Assembly assembly, string resourceName)
+    {
+        Guard.IsNotNull(assembly);
+        Guard.IsNotNullOrWhiteSpace(resourceName);
+
+        resourceName = ExpandResourceName(assembly, resourceName);
+        return assembly.GetManifestResourceStream(resourceName) ?? throw new InvalidOperationException();
+    }
+
+    /// <summary>
+    /// Read Embedded Resource Stream data from assembly.
+    /// </summary>
+    /// <param name="assembly">Assembly that contains the embedded resource stream.</param>
+    /// <param name="resourceName"></param>
+    /// <returns>The mebedded stream data.</returns>
+    public static byte[] ReadEmbeddedResourceData(this Assembly assembly, string resourceName)
+    {
+        Guard.IsNotNull(assembly);
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        Guard.IsNotNull(stream, nameof(resourceName));
+
+        var data = new byte[stream.Length];
+        stream.Read(data, 0, data.Length);
+        return data;
+    }
+
+    /// <summary>
+    /// Iterate lines of embedded resource stream.
+    /// </summary>
+    /// <param name="assembly">Assembly that contains the embedded resource stream.</param>
+    /// <param name="resourceName">Full or partial name of the resource.</param>
+    /// <param name="encoding">Encoding of embedded stream text content. If not provided UTF-8 is used.</param>
+    /// <returns>Enumerable </returns>
+    public static IEnumerable<string> ReadEmbeddedResourceLines(this Assembly assembly, string resourceName, Encoding? encoding = null)
+    {
+        Guard.IsNotNull(assembly);
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        Guard.IsNotNull(stream, nameof(resourceName));
+
+        encoding ??= Encoding.UTF8;
+        using var reader = new StreamReader(stream, encoding);
+
+        return reader.EnumLines();
+    }
+
+    /// <summary>
+    /// Iterate lines of embedded resource stream.
+    /// </summary>
+    /// <param name="assembly">Assembly that contains the embedded resource stream.</param>
+    /// <param name="resourceName">Full or partial name of the resource.</param>
+    /// <param name="encoding">Encoding of embedded stream text content. If not provided UTF-8 is used.</param>
+    /// /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns>Enumerable </returns>
+    public static async IAsyncEnumerable<string> ReadEmbeddedResourceLinesAsync(
+        this Assembly assembly,
+        string resourceName,
+        Encoding? encoding = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default
+    )
+    {
+        Guard.IsNotNull(assembly);
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        Guard.IsNotNull(stream, nameof(resourceName));
+
+        encoding ??= Encoding.UTF8;
+        using var reader = new StreamReader(stream, encoding);
+
+        await foreach(var line in reader.EnumLinesAsync(cancellationToken))
+        {
+            yield return line;
+        } 
+    }
+}
