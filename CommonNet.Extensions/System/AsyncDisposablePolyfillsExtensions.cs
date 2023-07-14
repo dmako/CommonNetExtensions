115b6dca0a49ee4299481e9b3de11a6bc69d4963
David Makovsky <david.makovsky@gmail.com>
2023-07-14 22:55:56 +0200

    feat: additional netstandard2.0 polyfills

---
+++ CommonNet.Extensions/System/AsyncDisposablePolyfillsExtensions.cs
@@ -0,0 +1,33 @@
+﻿#if NETSTANDARD2_0
+
+using CommunityToolkit.Diagnostics;
+
+namespace System;
+
+/// <summary>
+/// Netstandard 2.0 IDisposable polyfills for calling DisposeAsync()
+/// </summary>
+public static class AsyncDisposablePolyfillsExtensions
+{
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources asynchronously.
+    /// </summary>
+    /// <param name="disposable">The IDisposable object to perform the call on.</param>
+    /// <returns></returns>
+    public static ValueTask DisposeAsync(this IDisposable disposable)
+    {
+        Guard.IsNotNull(disposable);
+
+        try
+        {
+            disposable.Dispose();
+            return default;
+        }
+        catch (Exception ex)
+        {
+            return new ValueTask(Task.FromException(ex));
+        }
+    }
+}
+
+#endif
