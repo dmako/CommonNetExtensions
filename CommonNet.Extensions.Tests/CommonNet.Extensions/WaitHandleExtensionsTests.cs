b5b4f10f974b7ff2b091c4deaa7f9d6c0f33b3bc
David Makovsky <david.makovsky@gmail.com>
2023-07-25 23:21:53 +0200

    feat: wiat handle extensions

---
+++ CommonNet.Extensions.Tests/CommonNet.Extensions/WaitHandleExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CommonNet.Extensions.Tests;
+
+public class WaitHandleExtensionsTests
+{
+    [Fact]
+    public async Task AsTask_ShouldComplete_WhenWaitHandleIsSignaled()
+    {
+        using var manualResetEvent = new ManualResetEvent(false);
+
+        var task = manualResetEvent.AsTask();
+        task.IsCompleted.Should().BeFalse();
+
+        manualResetEvent.Set();
+        await task;
+
+        task.IsCompleted.Should().BeTrue();
+        task.Result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task AsTask_WithTimeout_ShouldComplete_WhenWaitHandleIsSignaled()
+    {
+        using var manualResetEvent = new ManualResetEvent(false);
+
+        var task = manualResetEvent.AsTask(TimeSpan.FromSeconds(1));
+        task.IsCompleted.Should().BeFalse();
+
+        manualResetEvent.Set();
+        await task;
+
+        task.IsCompleted.Should().BeTrue();
+        task.Result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task AsTask_WithTimeout_ShouldCancel_WhenTimeoutIsReached()
+    {
+        using var manualResetEvent = new ManualResetEvent(false);
+        var task = manualResetEvent.AsTask(TimeSpan.FromMilliseconds(100));
+
+        task.IsCompleted.Should().BeFalse();
+
+        Exception? exception = null;
+        try
+        {
+            await task;
+        }
+        catch (TaskCanceledException ex)
+        {
+            exception = ex;
+        }
+        catch (AggregateException ex) when (ex.InnerExceptions.Count == 1 && ex.InnerExceptions[0] is TaskCanceledException)
+        {
+            exception = ex;
+        }
+
+        exception.Should().NotBeNull();
+
+        task.IsCanceled.Should().BeTrue();
+    }
+}
---
+++ CommonNet.Extensions/System.Threading.Tasks/WaitHandleExtensions.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Diagnostics;
+
+namespace System.Threading.Tasks;
+
+/// <summary>
+/// Provides extension methods to convert WaitHandle instances to Task&lt;bool&gt; for asynchronous waiting.
+/// </summary>
+public static class WaitHandleExtensions
+{
+    /// <summary>
+    /// Converts a WaitHandle to a Task&lt;bool&gt; that completes when the WaitHandle is signaled.
+    /// </summary>
+    /// <param name="handle">The WaitHandle to convert.</param>
+    /// <returns>
+    /// A Task&lt;bool&gt; that completes with a value of true when the WaitHandle is signaled,
+    /// or false if the WaitHandle is disposed before it is signaled.
+    /// </returns>
+    public static Task<bool> AsTask(this WaitHandle handle)
+    {
+        return AsTask(handle, Timeout.InfiniteTimeSpan);
+    }
+
+    /// <summary>
+    /// Converts a WaitHandle to a Task&lt;bool&gt; that completes when the WaitHandle is signaled or when the specified timeout is reached.
+    /// </summary>
+    /// <param name="handle">The WaitHandle to convert.</param>
+    /// <param name="timeout">The maximum time to wait for the WaitHandle to be signaled.</param>
+    /// <returns>
+    /// A Task&lt;bool&gt; that completes with a value of true when the WaitHandle is signaled,
+    /// false if the WaitHandle is disposed before it is signaled, or canceled if the specified timeout is reached.
+    /// </returns>
+    public static Task<bool> AsTask(this WaitHandle handle, TimeSpan timeout)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        var registration = ThreadPool.RegisterWaitForSingleObject(handle, (state, timedOut) =>
+        {
+            Guard.IsNotNull(state);
+            
+            var localTcs = (TaskCompletionSource<bool>)state;
+            if (timedOut)
+            {
+                localTcs.TrySetCanceled();
+            }
+            else
+            {
+                localTcs.TrySetResult(true);
+            }
+        }, tcs, timeout, true);
+
+        tcs.Task.ContinueWith((_, state) => ((RegisteredWaitHandle)state!).Unregister(null), registration, TaskScheduler.Default);
+        return tcs.Task;
+    }
+}
